1a50c2d5771d77f86d14d1eaa06c7ced6d1d7552
dan <contact@danmyers.net>
2024-10-28 20:57:33 -0500

    updated wood bot

--- wood.lua
+++ wood.lua
@@ -1,232 +1,113 @@
-local width         --amount of trees wide of the tree farm
-local length        --amount of tree length of the tree farm
-local treeDistance  --the distance of blocks between
-local sleepTime     --the amount of time between the turtle checking the tree farm for grown trees
-local pos           --current distance from the starting point
-local direction     --current direction turtle is facing
+--
+-- Turtle Woodcutter Script
+--
 
-local function fuelCheck()
-    if turtle.getFuelLevel() == 0 then
-        for i=1,15,1 do
-            if turtle.getFuelLevel() > 0 then
-                break
-            end
-            
-            turtle.select(i)
+local saplingSlot = nil
+local fuelSlot = nil
 
-            if turtle.getItemDetail().name ~= "minecraft:oak_sapling" then
-                turtle.refuel(1)
+-- Function to find the slots for saplings and fuel
+local function findItems()
+    for slot = 1, 16 do
+        local itemDetail = turtle.getItemDetail(slot)
+        if itemDetail then
+            if itemDetail.name == "minecraft:oak_sapling" and not saplingSlot then
+                saplingSlot = slot
+            elseif itemDetail.name == "minecraft:coal" or itemDetail.name == "minecraft:charcoal" then
+                fuelSlot = slot
             end
         end
-    end
-
-    if turtle.getFuelLevel() < 1 then
-        print("OH FUCK OH SHIT, WE OUT OF FUEL ABORT")
-        os.exit()
-    end
-end
-
-local function up()
-    fuelCheck()
-    
-    if turtle.up() then
-        pos.y = pos.y + 1
-    end
-end
-
-local function down()
-    fuelCheck()
-
-    if turtle.down() then
-        pos.y = pos.y - 1
-    end
-end
-
-local function forward()
-    fuelCheck()
-
-    if turtle.forward() then
-        if direction == 0 then
-            pos.z = pos.z + 1
-        elseif direction == 1 then
-            pos.x = pos.x + 1
-        elseif direction == 2 then
-            pos.z = pos.z - 1
-        elseif direction == 3 then
-            pos.x = pos.x - 1
-        else
-            print("Oh shit what direction are we even facing???? OH FUCK OH GOD. ABORT")
-            os.exit()
+        if saplingSlot and fuelSlot then
+            break
         end
     end
 end
 
-local function left()
-    if turtle.turnLeft() then
-        direction = math.abs((direction - 1) % 4)
+-- Function to check if the turtle has saplings
+local function hasSaplings()
+    return saplingSlot ~= nil
+end
+
+-- Function to refuel the turtle
+local function refuel()
+    if turtle.getFuelLevel() < 1 and hasSaplings() then
+        turtle.select(fuelSlot)
+        turtle.refuel(1)
     end
 end
 
-local function right()
-    if turtle.turnRight() then
-        direction = math.abs((direction + 1) % 4)
-    end
-end
-
-local function moveToNextTree()
-    local i=0
-    while i <= treeDistance do
+-- Function to check and break the tree
+local function checkAndBreak()
+    if turtle.detect() then
         turtle.dig()
-        forward()
-        i = i + 1
     end
 end
 
-local function mineTree()
-    local success, data = turtle.inspectUp()
-
-    if success and data.name == "minecraft:oak_log" then
-        turtle.digDown()
-    end
-    
-    while success and data.name == "minecraft:oak_log" do
-        turtle.digUp()
-        up()
-        success, data = turtle.inspectUp()
-    end
-
-    local y = pos.y
-    while pos.y > 0 do
-        down()
-    end
-
-
-end
-
+-- Function to check if planting is possible and plant sapling
 local function plantSapling()
-    local i = 1
-    while i <= 16 do
-        turtle.select(i)
-        local itemDetails = turtle.getItemDetail()
-        if itemDetails ~= nil and itemDetails.name == "minecraft:oak_sapling" then
+    if saplingSlot then
+        turtle.select(saplingSlot)
+        if not turtle.detectDown() then
             turtle.placeDown()
-            break;
         end
-        i = i + 1
     end
 end
 
-local function turnAround(turnRight)
-    moveToNextTree()
-    if turnRight then
-        right()
-    else
-        left()
-    end
-    moveToNextTree()
-    if turnRight then
-        right()
-    else
-        left()
-    end
 
-    return not turnRight
+local function startup()
+    findItems()
+    for i = 1, 2 do
+        turtle.forward()
+    end
 end
 
-local function returnHome()
-    if pos.z == 0 then
-        left()
-    else
-        while pos.z > 0 do
-            forward()
+local function rowLeft()
+    turtle.turnLeft()
+    turtle.forward()
+    turtle.forward()
+    turtle.forward()
+    turtle.turnLeft()
+end
+
+local function rowRight()
+    turtle.turnRight()
+    turtle.forward()
+    turtle.forward()
+    turtle.forward()
+    turtle.turnRight()
+end
+
+local function checkRowDirection(row)
+    if row < 5 then
+        if row % 2 == 1 then
+            rowLeft()
+        else
+            rowRight()
         end
-    
-        right()
-    end
-
-    while pos.x > 0 do
-        forward()
-    end
-
-    right()
-end
-
-local function depositeWood()
-    while direction ~= 2 do
-        right()
-    end
-
-    local i = 1
-    while i <= 16 do
-        turtle.select(i)
-        local itemDetails = turtle.getItemDetail()
-        if itemDetails ~= nil and itemDetails.name == "minecraft:oak_log" then
-            turtle.drop()
-        end
-        i = i + 1
     end
 end
 
-local function restockSaplings()
-    while direction ~= 3 do
-        right()
+local function runThroughRow(slot)
+    if slot < 13 then
+        turtle.forward()
     end
-
-    local i = 1
-    while i <= 16 do
-        turtle.select(i)
-        local itemDetails = turtle.getItemDetail()
-        if itemDetails ~= nil and itemDetails.name == "minecraft:oak_sapling" then
-            turtle.drop()
-        end
-        i = i + 1
-    end
-
-    turtle.select(1)
-    turtle.suck()
-end
-
-local function init()
-    width = 5;
-    length = 5;
-    treeDistance = 1;
-    sleepTime = 1 --300 --Sleep Time is 5 minutes
-    pos = {
-        x=0,
-        y=0,
-        z=0,
-    }
-    direction = 0;
 end
 
 local function loop()
-    while true do
-        local turnRight = true;
-        restockSaplings()
-
-        while direction ~= 0 do
-            right()
+    for row = 1, 5 do
+        for slot = 1, 13 do
+            refuel()
+            checkAndBreak()
+            plantSapling()
+            runThroughRow(slot)
         end
 
-        local widDist=0
-        while widDist < width  do
-            local lenDist=0
-            while lenDist < length do
-                moveToNextTree()
-                mineTree()
-                plantSapling()
-                lenDist = lenDist + 1
-            end
-            
-            turnRight = turnAround(turnRight)
-            widDist = widDist + 1
-        end
-
-        returnHome()
-        depositeWood()
-        os.sleep(sleepTime)
+        checkRowDirection(row)
     end
 end
 
-init()
-loop()
+local function main()
+    startup()
+    loop()
+end
+
+main()
