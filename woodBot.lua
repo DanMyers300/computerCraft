e31e05a343ac81df5f497bbc4e220f82c5660fd4
dan <contact@danmyers.net>
2024-11-01 21:14:37 -0500

    fixed warnings

--- woodBot.lua
+++ woodBot.lua
@@ -5,6 +5,16 @@ local fuelSlot = nil
 local slotHasSapling = {}
 local slotHasFuel = {}
 local slotHasJunk = {}
+--local detectedTree
+
+local function includes(table, item)
+    for _, value in ipairs(table) do
+        if value == item then
+            return true
+        end
+    end
+    return false
+end
 
 local function findItems()
     for _ = 1, 16 do
@@ -21,24 +31,19 @@ local function findItems()
                     fuelSlot = _
                 end
                 table.insert(slotHasFuel, _)
-            elseif itemDetail.name ~= "minecraft:coal" or itemDetail.name ~= "minecraft:charcoal" or itemDetail.name ~= "minecraft:oak_sapling" then
-                table.insert(slotHasJunk, _)
-            else 
-                if includes(slotHasSapling, _) then table.remove(slothasSapling, _) end
+            elseif itemDetail.name ~= "minecraft:coal" then
+                if itemDetail.name ~= "minecraft:charcoal" then
+                    if itemDetail.name ~= "minecraft:oak_sapling" then
+                        table.insert(slotHasJunk, _)
+                    end
+                end
+            else
+                if includes(slotHasSapling, _) then table.remove(slotHasSapling, _) end
                 if includes(slotHasFuel, _) then table.remove(slotHasFuel, _) end
                 if includes(slotHasJunk, _) then table.remove(slotHasJunk, _) end
             end
         end
     end
-end
-
-local function includes(table, item)
-    for _, value in ipairs(table) do
-        if value == item then
-            return true
-        end
-    end
-    return false
 end
 
 local function refuel()
@@ -50,7 +55,7 @@ local function refuel()
 
     if turtle.getFuelLevel() < 1 then
         turtle.select(fuelSlot)
-        local _, err turtle.refuel(1); if err then error(err) end
+        local _, err = turtle.refuel(1); if not _ then error(err) end
     end
 end
 
@@ -62,42 +67,42 @@ local function moveForward(blocks)
     end
 end
 
-local function checkAndBreak()
-    if turtle.detect() then
-        local _, err turtle.dig()
-        if err then error(err) end
-    end
-end
+--local function checkAndBreak()
+--    local _, info
+--    _, info = turtle.inspect()
+--    if _ then
+--        if type(info) == table then
+--            if info.name == "minecraft:oak_log" then
+--                detectedTree = true
+--            end
+--            turtle.dig()
+--        else
+--            error(info)
+--        end
+--    end
+--end
 
 local function plantSapling()
     if saplingSlot then
         turtle.select(saplingSlot)
         if not turtle.detectDown() then
-            local _, err turtle.placeDown(); if err then error(err) end
+            local _, err = turtle.placeDown(); if not _ then error(err) end
         end
     else
         error('no saplings')
     end
 end
 
-local function rowLeft()
-    turtle.turnLeft()
-    moveForward(3)
-    turtle.turnLeft()
-end
-
-local function rowRight()
-    turtle.turnRight()
-    moveForward(3)
-    turtle.turnRight()
-end
-
 local function checkRowDirection(row)
     if row < 5 then
         if row % 2 == 1 then
-            rowLeft()
+            turtle.turnLeft()
+            moveForward(3)
+            turtle.turnLeft()
         else
-            rowRight()
+            turtle.turnRight()
+            moveForward(3)
+            turtle.turnRight()
         end
     end
 end
@@ -155,7 +160,7 @@ local function dumpJunk()
         for slot = 1, 16 do
             if includes(slotHasJunk, slot) then
                 turtle.select(slot)
-                _, err = turtle.dropDown()
+                local _, err = turtle.dropDown()
                 if err then print(err) end
             end
         end
@@ -168,7 +173,6 @@ local function startup()
     findItems()
     print("Sapling Slot: " .. tostring(saplingSlot))
     print("Fuel Slot: " .. tostring(fuelSlot))
-
     refuel()
     ensureSapling()
     findItems()
@@ -180,7 +184,6 @@ local function startup()
 end
 
 local function returnToHome()
-
     turtle.turnLeft()
     moveForward(2)
     turtle.turnLeft()
@@ -195,7 +198,6 @@ local function loop()
     for row = 1, 5 do
         for block = 1, 13 do
             refuel()
-            checkAndBreak()
             plantSapling()
             runThroughRow(block)
         end
